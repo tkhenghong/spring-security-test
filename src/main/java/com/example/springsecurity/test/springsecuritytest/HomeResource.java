3035fc45ed82013c04af987279c598c9d0a2bf2a
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-03-12 07:23:10 +0800

    Init commit

---
+++ src/main/java/com/example/springsecurity/test/springsecuritytest/HomeResource.java
@@ -0,0 +1,18 @@
+package com.example.springsecurity.test.springsecuritytest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    // http://localhost:8080/
+    // Will auto redirected to http://localhost:8080/login.
+    // To sign in, enter
+    // Username: user
+    // Password: 430cdbac-0ddb-414a-8457-12bbc62e3822 (Go to your Spring Boot log and find the line: "Using generated security password:......"
+    @GetMapping("/")
+    public String home() {
+        return "<h1>Welcome</h1>";
+    }
+}
