196b6b97dc17c4cb1bc378d66240a2f2903f2487
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-03-12 08:21:44 +0800

    Added in memory authentication in SecurityConfiguration

--- src/main/java/com/example/springsecurity/test/springsecuritytest/HomeResource.java
+++ src/main/java/com/example/springsecurity/test/springsecuritytest/HomeResource.java
@@ -11,6 +11,9 @@ public class HomeResource {
     // To sign in, enter
     // Username: user
     // Password: 430cdbac-0ddb-414a-8457-12bbc62e3822 (Go to your Spring Boot log and find the line: "Using generated security password:......"
+    // When you set your username and password in application.properties file, you enter your own username and password.
+    // When using proper file like create a class called SecurityConfiguration extends WebSecurityConfigurerAdapter.
+    // In memory authentication, you also enter your own username and password.
     @GetMapping("/")
     public String home() {
         return "<h1>Welcome</h1>";
---
+++ src/main/java/com/example/springsecurity/test/springsecuritytest/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.example.springsecurity.test.springsecuritytest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+// Create a class and extends WebSecurityConfigurerAdapter
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Hide the super.configure(auth) below?
+        // super.configure(auth);
+
+        // For example, if I want to do memory authentication, you can configure it like this.
+        // method chaining: You can call a method behind a method and so on.
+        auth.inMemoryAuthentication().withUser("blah").password("blah").roles("USER");
+        // There's Method level security (Not taught)
+    }
+
+    // You can create this bean anywhere. And Spring Security will auto using yours as default PasswordEncoder
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        // This NoOpPasswordEncoder object is deprecated is not because it's expired, but it's the Spring Security team
+        // don't want to recommend you to use this type of password encoder in your application.
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
